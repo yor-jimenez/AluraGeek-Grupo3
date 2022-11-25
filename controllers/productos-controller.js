3bc891cfd16d085e66c3742f8edbf7b9e9bf94a8
yor-jimenez <yorleyuribe@gmail.com>
2022-11-25 14:44:39 -0300

    arreglando stylos

--- controllers/productos-controller.js
+++ controllers/productos-controller.js
@@ -1,40 +1,35 @@
-import { productoServices } from "../servicios/producto-servicios.js";
-import { formatPrice } from "../formatterPrices.js";
+import { productoServices } from "../servicios/productos-servicios.js";
 
-const nuevoProduto = (name, price, imageUrl, id) => {
-  const card = document.createElement("div");
-  const contenido = `
-        <div class="produto">
-            <img src="${imageUrl}" alt="img">
-            <h1 class="product-name"> ${name} </h1>
-            <p class="preco">${formatPrice(price)}</p>
-            <a class="ver-produto" href="../produto.html?id=${id}">Ver Produto</a>
-        </div>   
+
+const nuevoProducto = (name,price,imageUrl) => {
+ 
+    const card = document.createElement("div");
+    const contenido =  `
+       <div>
+            <img src="${imageUrl}" alt="">
+            <h3>${name}</h3>
+            <p>${price}</p>
+           
+        </div> 
     `;
-  card.innerHTML = contenido;
-  card.dataset.id = id;
-
-  return card;
+    card.innerHTML = contenido;
+    card.classList.add("card")
+    return card; 
 };
 
-const productos = document.querySelector("[data-product]");
+const productos = document.querySelector("[datos-productos]")
 
-const render = async () => {
-  try {
-    const listaProductos = await productoServices.listaProductos();
-    listaProductos.forEach((elemento) => {
-      productos.appendChild(
-        nuevoProduto(
-          elemento.name,
-          elemento.price,
-          elemento.imageUrl,
-          elemento.id
-        )
-      );
-    });
-  } catch (erro) {
-    console.log(erro);
-  }
-};
+const render = async () =>  {
+    try {
+        const listaProductos = await productoServices.listaProductos()
+        listaProductos.forEach(elemento => {
+            productos.appendChild(nuevoProducto(elemento.name,elemento.price, elemento.imageUrl))
+    })
+    }
+    catch(erro){
+        console.log(erro)
+    }
+    
+}
 
-render();
+render()
